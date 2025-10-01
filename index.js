4c83c16dc1c3ba27af3acd992a6b9f0de984c813
Branice Simaloi <simaloibranice@gmail.com>
2025-10-02 01:18:21 +0300

    Implement Formatter class with static methods

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]/g, '');
+  }
+
+  static titleize(sentence) {
+    const exceptions = [
+      "the", "a", "an", "but", "of", "and", 
+      "for", "at", "by", "from"
+    ];
+
+    return sentence
+      .toLowerCase()
+      .split(" ")
+      .map((word, index) => {
+        if (index === 0 || !exceptions.includes(word)) {
+          return this.capitalize(word);
+        } else {
+          return word;
+        }
+      })
+      .join(" ");
+  }
 }
